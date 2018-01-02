5025b57e5a63e8e262ad377129064f01aed0d6a8
petrubear <edison.martinez@gmail.com>
2018-01-02 16:43:18 -0500

    add hystrix-fallback-bulkhead support

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -4,19 +4,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @RefreshScope
+@EnableEurekaClient
+@EnableHystrix  //si esto no se agrega no funcionan los circuit breakers y no muestra error
 public class Application {
-    public static void main(String... args) {
-        SpringApplication.run(Application.class, args);
-    }
-
     @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    public static void main(String... args) {
+        SpringApplication.run(Application.class, args);
     }
 }
--- licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/services/LicenseService.java
@@ -1,5 +1,7 @@
 package com.thoughtmechanix.licenses.services;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import com.thoughtmechanix.licenses.clients.OrganizationRestTemplateClient;
 import com.thoughtmechanix.licenses.config.ServiceConfig;
 import com.thoughtmechanix.licenses.model.License;
@@ -8,7 +10,9 @@ import com.thoughtmechanix.licenses.repository.LicenseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 @Service
@@ -23,12 +27,46 @@ public class LicenseService {
     @Autowired
     OrganizationRestTemplateClient organizationRestClient;
 
-//    public License getLicense(String organizationId, String licenseId) {
-//        License license = licenseRepository.findByOrganizationIdAndLicenseId(organizationId, licenseId);
-//        return license.withComment(config.getExampleProperty());
-//    }
+    /**
+     * Metodo para simular el timeout para pruebas de hystrix
+     */
 
+    private void randomlyRunLong() {
+        Random rand = new Random();
+        int randomNum = rand.nextInt((3 - 1) + 1) + 1;
+        if (randomNum == 3) {
+            sleep();
+        }
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(11000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @HystrixCommand(fallbackMethod = "buildFallbackLicenseList",
+            //configuracion del pool independiente para esta llamada
+            //por defecto hystrix usa el mismo pool de 10 para todas las llamadas
+            threadPoolKey = "licenseByOrgThreadPool",
+            threadPoolProperties =
+                    {@HystrixProperty(name = "coreSize", value = "30"),
+                            @HystrixProperty(name = "maxQueueSize", value = "10")},
+            commandProperties = {
+                    //timeout de la llamada hystrix
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000"),
+                    //configuracion del circuit breaker
+                    @HystrixProperty(name="circuitBreaker.requestVolumeThreshold", value="10"),
+                    @HystrixProperty(name="circuitBreaker.errorThresholdPercentage", value="75"),
+                    @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds", value="7000"),
+                    @HystrixProperty(name="metrics.rollingStats.timeInMilliseconds", value="15000"),
+                    @HystrixProperty(name="metrics.rollingStats.numBuckets", value="5")
+            })
     public List<License> getLicensesByOrg(String organizationId) {
+        randomlyRunLong();
         return licenseRepository.findByOrganizationId(organizationId);
     }
 
@@ -52,15 +90,34 @@ public class LicenseService {
 
         Organization org = getOrganization(organizationId);
 
-        return license
-                .withOrganizationName(org.getName())
+        return license.withOrganizationName(org.getName())
                 .withContactName(org.getContactName())
                 .withContactEmail(org.getContactEmail())
                 .withContactPhone(org.getContactPhone())
                 .withComment(config.getExampleProperty());
     }
 
+    @HystrixCommand(
+            threadPoolKey = "getOrganizationPool",
+            threadPoolProperties =
+                    {@HystrixProperty(name = "coreSize", value = "30"),
+                            @HystrixProperty(name = "maxQueueSize", value = "10")},
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "500")
+            })
     private Organization getOrganization(String organizationId) {
         return organizationRestClient.getOrganization(organizationId);
+    }
+
+    //fallback debe estar en el mismo archivo donde se define el HystrixCommand y debe tener los mismos parametros
+    private List<License> buildFallbackLicenseList(String organizationId) {
+        List<License> fallbackList = new ArrayList<>();
+        License license = new License()
+                .withId("0000000-00-00000")
+                .withOrganizationId(organizationId)
+                .withProductName("Sorry no licensing information currently available");
+
+        fallbackList.add(license);
+        return fallbackList;
     }
 }
