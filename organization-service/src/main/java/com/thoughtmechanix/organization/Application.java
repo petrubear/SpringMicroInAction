5d1360b23d5112304927587db1d266fe57521379
petrubear <edison.martinez@gmail.com>
2018-01-13 18:11:05 -0500

    add oauth to organization service

--- organization-service/src/main/java/com/thoughtmechanix/organization/Application.java
+++ organization-service/src/main/java/com/thoughtmechanix/organization/Application.java
@@ -6,12 +6,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 import javax.servlet.Filter;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableResourceServer
 public class Application {
 
     @Bean
